f752c93e0e7cdfb5625bc5048039be2bd2f2771c
Luis <luis.campos.escobar@gmail.com>
2021-07-02 23:21:40 -0400

    Respaldando lo realizado en github

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//cambiar color navbar segun posicion scroll
+$(window).scroll(function () {
+  let pos = $(window).scrollTop(); //obtengo posicion scroll
+  if (pos < 100) {
+    // comparo si es menor a 100px
+    $("nav").addClass("bg-transparent"); //añado clase bg-dark
+    $("nav").removeClass("navbar-negro"); //quito clase bg-transparent
+  } else {
+    //si es mayor o igual a 100px la posicion de scroll
+    $("nav").removeClass("bg-transparent"); // quito la clase bg-dark
+    $("nav").addClass("navbar-negro"); //añado la clase bg-transparent
+  }
+});
+
+// INCORPORANDO SMOOTHSCROLL
+$(document).ready(function () {
+  $("a").click(function (event) {
+    event.preventDefault();
+    var gato = this.hash;
+
+    $("html").animate(
+      {
+        scrollTop: $(gato).offset().top - 57,
+      },
+      800
+    );
+  });
+});
+// INCORPORANDO TOOLTIP BOOTSTRAP 4.6
+$('[data-toggle="tooltip"]').tooltip();
+// INCORPORANDO POPOVER BOOTSTRAP 4.6
+$('[data-toggle="popover"]').popover();
